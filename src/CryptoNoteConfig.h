41aaee2ba5fc87213335d9622834f8a4dd4f4e26
Mikkelhauvgaard <mikkelhauvgaard@gmail.com>
2018-02-07 20:59:56 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,8 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "menjicoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001a0989ae828029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210114ad18af7184976fda34cb899cc578f28aeadbc32e6d10e3bd04776dc8afeeb7";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001a0989ae828029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101f743f56f9637d24306f23573784ecaa8ef1ef28cc594cd2709e95092d021f0de";
+
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
